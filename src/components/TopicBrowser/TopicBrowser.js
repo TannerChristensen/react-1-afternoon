f34dd60a3ad7959da52f0bc7bf2f4e5a00b14dd8
Tanner Christensen <tannerchristensen20@gmail.com>
2020-10-20 22:07:24 -0600

    did as much as i could, my brain is imploding

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,24 @@
+import EvenAndOdd from '../Topics/EvenAndOdd'
+import FilterObject from '../Topics/FilterObject'
+import FilterString from '../Topics/FilterString'
+import Palindrome from '../Topics/Palindrome'
+import Sum from '../Topics/Sum'
+
+
+import React, { Component } from 'react';
+
+class TopicBrowser extends Component {
+    render(){
+        return (
+            <div>
+        <EvenAndOdd />
+        <FilterObject />
+        <FilterString />
+        <Palindrome />
+        <Sum />
+      </div>
+        )
+    }
+}
+
+export default TopicBrowser;
